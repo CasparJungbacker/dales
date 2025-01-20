1dc92dc5757efa3ac87c7e86d334eabc4cb6e238
Caspar Jungbacker <caspar.jungbacker@outlook.com>
2025-01-20 17:39:20 +0100

    Tracers: remove svprof from GPU

--- src/modtracers.f90
+++ src/modtracers.f90
@@ -220,7 +220,7 @@ contains
     !$acc enter data copyin(svm(2-ih:i1+ih,2-jh:j1+jh,1:k1,1:nsv), &
     !$acc&                  sv0(2-ih:i1+ih,2-jh:j1+jh,1:k1,1:nsv), &
     !$acc&                  svp(2-ih:i1+ih,2-jh:j1+jh,1:k1,1:nsv), &
-    !$acc&                  sv0av(1:k1,1:nsv), svprof(1:k1,1:nsv))
+    !$acc&                  sv0av(1:k1,1:nsv))
 
   end subroutine allocate_tracers
 
@@ -230,7 +230,7 @@ contains
     !$acc exit data delete(svm(2-ih:i1+ih,2-jh:j1+jh,1:k1,1:nsv), &
     !$acc&                 sv0(2-ih:i1+ih,2-jh:j1+jh,1:k1,1:nsv), &
     !$acc&                 svp(2-ih:i1+ih,2-jh:j1+jh,1:k1,1:nsv), &
-    !$acc&                 sv0av(1:k1,1:nsv), svprof(1:k1,1:nsv))
+    !$acc&                 sv0av(1:k1,1:nsv))
 
     if (nsv > 0) then
       deallocate(tracer_prop)
