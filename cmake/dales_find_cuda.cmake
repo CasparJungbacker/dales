1c66831c076c9f777ed3192f9fdbc53f0963600b
Caspar Jungbacker <caspar.jungbacker@outlook.com>
2026-01-21 17:56:07 +0100

    CMake: add support for building CUDA sources

--- cmake/dales_find_cuda.cmake
+++ cmake/dales_find_cuda.cmake
@@ -1,9 +1,21 @@
+include( CheckLanguage )
+
 macro( dales_find_cuda )
   # Check if a target architecture has been set. If not, default to 80 (A100 & RTX3090)
   # For H100, use 90
   if( NOT DEFINED CMAKE_CUDA_ARCHITECTURES )
     set( CMAKE_CUDA_ARCHITECTURES 80 )
   endif()
+
+	# Try to enable CUDA
+	check_language( CUDA )
+
+	if( CMAKE_CUDA_COMPILER )
+		enable_language( CUDA )
+		add_compile_definitions( USE_CUDA )
+  else()
+		ecbuild_info( "Could not enable CUDA" )
+	endif()
   
   # Look for the CUDA Toolkit, which has cuFFT and NVTX
   find_package( CUDAToolkit )
@@ -17,5 +29,6 @@ macro( dales_find_cuda )
   else()
     ecbuild_info( "Found NVTX: ${CUDA_nvToolsExt_LIBRARY}" )
   endif() 
+	
 endmacro()
 
